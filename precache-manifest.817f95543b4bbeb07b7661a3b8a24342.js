eb259b5f25e4df1ba501bfb9edf575f5f3dbdb0b
aefuen1 <aefuen1@gmail.com>
2020-09-15 23:49:04 -0400

    Updates

---
+++ precache-manifest.817f95543b4bbeb07b7661a3b8a24342.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f6de5fd842c19c1d362961eb6cc059",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "5919a478a8b020746b31",
+    "url": "/random-quote-machine/static/css/main.c1071d7c.chunk.css"
+  },
+  {
+    "revision": "bfcc8918d3c131ff005e",
+    "url": "/random-quote-machine/static/js/2.0059c9a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.0059c9a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5919a478a8b020746b31",
+    "url": "/random-quote-machine/static/js/main.5128a8c2.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
